d23a8ffda95035a562a7dc496a497e7888ce7887
Emily Ryznar <emily.ryznar@nmfs.local>
2024-09-04 10:41:18 -0800

    adding libs/params

--- Scripts/load_libs_params.R
+++ Scripts/load_libs_params.R
@@ -11,7 +11,14 @@ library(mice)
 library(Hmisc)
 library(ggmap)
 library(mgcv)
+library(MARSS)
+library(corrplot)
 
+# SET THEME ------
+theme_set(theme_bw())
+
+
+cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # LOAD UNIVERSAL PARAMS ------
 current.year <- 2024
